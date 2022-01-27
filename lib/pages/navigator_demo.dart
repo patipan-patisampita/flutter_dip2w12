4adc50df1c8600e5362f6e03ca0738ae53d2424c
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-27 13:05:18 +0700

    update

--- lib/pages/navigator_demo.dart
+++ lib/pages/navigator_demo.dart
@@ -17,52 +17,70 @@ class NavigatorDemo extends StatelessWidget {
         child: Icon(Icons.access_alarm),
       ),
       drawer: Drawer(
+        backgroundColor: Colors.indigo,
         child: ListView(
           children: [
             DrawerHeader(
-              child: Center(
-                child: Text(
-                  "HEADER",
-                  style: TextStyle(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage("images/thanos2.jpg"),
+              )),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
                       color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                ),
+                    ),
+                    child: Center(child: Text("Home",style: TextStyle(fontSize: 20))),
+                  ),
+                  Text("Thanos:Infinity War",style: TextStyle(color: Colors.white)),
+                ],
               ),
-              decoration: BoxDecoration(color: Colors.indigo),
             ),
             ListTile(
-                title: Text("MENU-1"),
-                leading: Icon(Icons.home),
+                title: Text("MENU-1",style: TextStyle(color: Colors.white)),
+                leading: Icon(Icons.home,color: Colors.orange),
                 onTap: () {
                   print("MENU-1");
+                  Navigator.pop(context);
                 }),
             ListTile(
-                title: Text("MENU-2"),
-                leading: Icon(Icons.accessibility_outlined),
+                title: Text("MENU-2",style: TextStyle(color: Colors.white)),
+                leading: Icon(Icons.accessibility_outlined,color: Colors.green),
                 onTap: () {
                   print("MENU-2");
+                  Navigator.pop(context);
                 }),
             ListTile(
-              title: Text("MENU-3"),
-              leading: Icon(Icons.api_outlined),
+              title: Text("MENU-3",style: TextStyle(color: Colors.white)),
+              leading: Icon(Icons.api_outlined,color: Colors.green),
               onTap: () {
                 print("MENU-3");
+                Navigator.pop(context);
               },
             ),
             ListTile(
-              title: Text("MENU-4"),
-              leading: Icon(Icons.important_devices_outlined),
+              title: Text("MENU-4",style: TextStyle(color: Colors.white)),
+              leading: Icon(Icons.important_devices_outlined,color: Colors.green),
               onTap: () {
                 print("MENU-4");
+                Navigator.pop(context);
               },
             ),
+            SizedBox(height: 20),
+            Divider(color: Colors.grey),
             ListTile(
-                title: Text("Exit Program"),
-                leading: Icon(Icons.logout),
-            onTap: () {
-              print("Exit Program");
-            }),
+                title: Text("Exit Program",style: TextStyle(color: Colors.white)),
+                leading: Icon(Icons.logout,color: Colors.red),
+                onTap: () {
+                  print("Exit Program");
+                  Navigator.pop(context);
+                }),
           ],
         ),
       ),
