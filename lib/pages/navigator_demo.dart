dd43cc028dcfe7eb184b465ed4692bd5a90855d6
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-27 10:55:16 +0700

    update

--- lib/pages/navigator_demo.dart
+++ lib/pages/navigator_demo.dart
@@ -31,11 +31,38 @@ class NavigatorDemo extends StatelessWidget {
               ),
               decoration: BoxDecoration(color: Colors.indigo),
             ),
-            ListTile(title: Text("MENU-1"),leading: Icon(Icons.home)),
-            ListTile(title: Text("MENU-2"),leading: Icon(Icons.accessibility_outlined)),
-            ListTile(title: Text("MENU-3"),leading: Icon(Icons.api_outlined)),
-            ListTile(title: Text("MENU-4"),leading: Icon(Icons.important_devices_outlined)),
-            ListTile(title: Text("MENU-5"),leading: Icon(Icons.settings_input_antenna_outlined)),
+            ListTile(
+                title: Text("MENU-1"),
+                leading: Icon(Icons.home),
+                onTap: () {
+                  print("MENU-1");
+                }),
+            ListTile(
+                title: Text("MENU-2"),
+                leading: Icon(Icons.accessibility_outlined),
+                onTap: () {
+                  print("MENU-2");
+                }),
+            ListTile(
+              title: Text("MENU-3"),
+              leading: Icon(Icons.api_outlined),
+              onTap: () {
+                print("MENU-3");
+              },
+            ),
+            ListTile(
+              title: Text("MENU-4"),
+              leading: Icon(Icons.important_devices_outlined),
+              onTap: () {
+                print("MENU-4");
+              },
+            ),
+            ListTile(
+                title: Text("Exit Program"),
+                leading: Icon(Icons.logout),
+            onTap: () {
+              print("Exit Program");
+            }),
           ],
         ),
       ),
